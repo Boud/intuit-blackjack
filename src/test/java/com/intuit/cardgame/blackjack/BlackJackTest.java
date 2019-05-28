69ca7bb0cfe8b623ffe8e1fd3d77e25e56253dc1
Boud <mouradnajar84@gmail.com>
2019-05-28 16:24:25 +0200

    Added more tests

--- src/test/java/com/intuit/cardgame/blackjack/BlackJackTest.java
+++ src/test/java/com/intuit/cardgame/blackjack/BlackJackTest.java
@@ -1,6 +1,12 @@
 package com.intuit.cardgame.blackjack;
 
+import com.intuit.cardgame.blackjack.players.BlackJackPlayer;
+import com.intuit.cardgame.blackjack.states.DealingState;
+import com.intuit.cardgame.common.Player;
+import com.intuit.cardgame.common.cards.Deck;
 import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BlackJackTest {
 
@@ -9,10 +15,29 @@ public class BlackJackTest {
     @Before
     public void init() {
         blackJack = new BlackJack();
+        blackJack.initGame();
     }
 
-//    @Test
-//    public void play(){
-//        blackJack.play();
-//    }
+    @Test
+    public void bustPlayer(){
+        blackJack.setCurrentState(new DealingState());
+        blackJack.getCurrentState().handle(blackJack);
+        BlackJackPlayer player = (BlackJackPlayer) blackJack.getCurrentPlayer();
+        assertNotNull(player);
+        while(player.getHandValue()<= 21){
+            blackJack.hit(player);
+        }
+        assertTrue(player.isBusted());
+    }
+
+    @Test
+    public void stand(){
+        blackJack.setCurrentState(new DealingState());
+        blackJack.getCurrentState().handle(blackJack);
+        BlackJackPlayer player = (BlackJackPlayer) blackJack.getCurrentPlayer();
+        assertNotNull(player);
+        blackJack.stand(player);
+        assertTrue(player.isEndedTurn());
+    }
+
 }
